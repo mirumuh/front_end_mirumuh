964863df76610244f07f6ec1c86d10b8982f5e5f
Your Name <you@example.com>
2024-11-13 14:14:47 -0300

    ajuste home

--- app/receitas/page.js
+++ app/receitas/page.js
@@ -1,10 +1,8 @@
-import Header from '../components/Header'
 import Receita from '../components/Receita'
 
 const Receitas = () => {
   return (
-    <div className='min-h-screen w-full'>
-      <Header />
+    <div className='w-full'>
       <div className='flex flex-col gap-10 w-full px-40 py-20 overflow-y-auto max-h-screenHeader'>
         <Receita />
         <Receita />
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,22 @@ module.exports = {
       maxHeight: {
         screenHeader: '70vh',
       },
+      height: {
+        screenHeader: '70vh',
+        96: '24rem',
+        128: '32rem',
+        144: '36rem',
+        160: '40rem',
+        192: '48rem',
+      },
+      minHeight: {
+        screenHeader: '70vh',
+        96: '24rem',
+        128: '32rem',
+        144: '36rem',
+        160: '40rem',
+        192: '48rem',
+      },
     },
   },
   plugins: [],
