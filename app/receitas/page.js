3ddc8635d57d8cc8b78dfb382bb7bc63411d476e
Julia Fazenda Ruiz <juliafazendaruiz@gmail.com>
2024-12-30 19:21:55 -0300

    commit teste

--- app/receitas/page.js
+++ app/receitas/page.js
@@ -4,7 +4,7 @@ import { mockDataReceitas } from '../../services/receitasData'
 
 const Receitas = () => {
   return (
-    <div className='w-full h-screen overflow-y-auto'>
+    <div className='w-full h-screenHeader overflow-y-auto'>
       <div className='flex flex-col gap-5 px-5 py-5 sm:py-10 md:px-20 lg:px-40 lg:py-14'>
         {mockDataReceitas.map(receita => (
           <Receita
