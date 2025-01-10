e8940b9c806c66f66ba7e9cb3a965dc19d5975b2
Your Name <you@example.com>
2025-01-10 09:54:16 -0300

    inicio da pagina de sucesso/cancelado

---
+++ app/sucesso/page.js
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Sucesso = () => {
+  return (
+    <div className='w-full h-screenHeader overflow-y-auto py-10 px-5 lg:py-8 lg:px-96 flex items-center justify-center'>
+      <div className='bg-white rounded-3xl shadow-lg flex gap-6 justify-center items-center py-5 px-6 md:px-8'>
+        <div className='flex'>
+          <Image
+            src={'/icons/logo_marcado.png'}
+            alt={'Icone da Mirumuh'}
+            width={300}
+            height={300}
+          ></Image>
+        </div>
+        <div className='flex flex-col justify-center items-center gap-6 px-10'>
+          <div className='flex justify-center items-center'>
+            <h2 className='text-[22px] font-semibold'>
+              Obrigada por comprar na Mirumuh!
+            </h2>
+          </div>
+          <div className='flex justify-center items-center '>
+            <p className='text-[16px] text-justify'>
+              Verifique seu e-mail para confirmar o recebimento do PDF.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sucesso
