825ea2d6d4c0a167d4789ba48681cd991c9d0082
Julia Fazenda Ruiz <109627889+jufazenda@users.noreply.github.com>
2025-02-19 17:39:50 -0300

    alterando pra back prod

--- services/api.js
+++ services/api.js
@@ -7,5 +7,5 @@ export const api = axios.create({
     Authorization: `Bearer ${configValue}`,
     'Content-Type': 'application/json',
   },
-  baseURL: 'http://localhost:8080/',
+  baseURL: 'http://159.89.52.201:8080/',
 })
