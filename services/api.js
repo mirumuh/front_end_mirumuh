67bfb80bc49880fa0d6c940260b1a99e8711f628
Julia Fazenda Ruiz <109627889+jufazenda@users.noreply.github.com>
2025-02-19 22:05:28 -0300

    ajuste api url

--- services/api.js
+++ services/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
 const configValue = process.env.NEXT_STRIPE_API_KEY_SECRET
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
+const testeUrl = process.env.NEXT_PUBLIC_TESTE_URL
 
 export const api = axios.create({
   headers: {
     Authorization: `Bearer ${configValue}`,
     'Content-Type': 'application/json',
   },
-  baseURL: 'https://api.mirumuh.com.br/',
+  baseURL: baseUrl,
 })
