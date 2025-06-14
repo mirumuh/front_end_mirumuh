0d630b126aba7111b1aeb2fe658916ab4f4ed674
Julia Fazenda Ruiz <juliafazendaruiz@gmail.com>
2025-06-14 15:36:06 -0300

    versao produto home

--- app/home/page.js
+++ app/home/page.js
@@ -43,52 +43,59 @@ const HomePage = () => {
           <div className='flex flex-col justify-center items-center gap-5 px-5 py-5 sm:py-10 md:px-20 lg:px-40 lg:py-14'>
             <div className='bg-white rounded-3xl shadow-lg p-6 w-full max-w-5xl flex flex-col items-center'>
               <div className='w-full bg-blue p-4 rounded-lg shadow-lg mb-2'>
-                <Swiper
-                  pagination={{ clickable: true }}
-                  autoplay={{
-                    delay: 3000,
-                    disableOnInteraction: false,
-                  }}
-                  modules={[Pagination, Autoplay]}
-                  className='rounded-lg overflow-hidden flex flex-col items-center justify-center'
-                >
-                  {slides.map((slide, index) => (
-                    <SwiperSlide
-                      key={index}
-                      className='px-2 py-4 w-full flex flex-col items-center justify-center'
-                    >
-                      <div className='flex flex-col items-center gap-2 w-full text-center'>
-                        {slide.image ? (
-                          <Image
-                            src={slide.image}
-                            alt={slide.name}
-                            width={250}
-                            height={250}
-                            className='object-cover rounded-3xl'
-                          />
-                        ) : null}
+                {slides.length > 0 ? (
+                  <Swiper
+                    pagination={{ clickable: true }}
+                    autoplay={{
+                      delay: 3000,
+                      disableOnInteraction: false,
+                    }}
+                    modules={[Pagination, Autoplay]}
+                    className='rounded-lg overflow-hidden flex flex-col items-center justify-center'
+                  >
+                    {slides.map((slide, index) => (
+                      <SwiperSlide
+                        key={index}
+                        className='px-2 py-4 w-full flex flex-col items-center justify-center'
+                      >
+                        <div className='flex flex-col items-center gap-2 w-full text-center'>
+                          {slide.image ? (
+                            <Image
+                              src={slide.image}
+                              alt={slide.name}
+                              width={250}
+                              height={250}
+                              className='object-cover rounded-3xl'
+                            />
+                          ) : null}
 
-                        <h2 className='text-xl font-semibold text-light-darker-brown'>
-                          {slide.name}
-                        </h2>
+                          <h2 className='text-xl font-semibold text-light-darker-brown'>
+                            {slide.name}
+                          </h2>
 
-                        <p className='font-bold text-md text-brown'>
-                          {(slide.prices[0].amount / 100)?.toLocaleString(
-                            'pt-BR',
-                            {
+                          <p className='font-bold text-md text-brown'>
+                            {(
+                              slide.prices[0].amount / 100
+                            )?.toLocaleString('pt-BR', {
                               style: 'currency',
                               currency: 'BRL',
-                            }
-                          )}
-                        </p>
+                            })}
+                          </p>
 
-                        <p className='text-gray-500 text-sm pb-4'>
-                          {slide.metadata.tipo}
-                        </p>
-                      </div>
-                    </SwiperSlide>
-                  ))}
-                </Swiper>
+                          <p className='text-gray-500 text-sm pb-4'>
+                            {slide.metadata.tipo}
+                          </p>
+                        </div>
+                      </SwiperSlide>
+                    ))}
+                  </Swiper>
+                ) : (
+                  <div className='flex justify-center items-center h-64'>
+                    <p className='text-gray-500'>
+                      Nenhum produto disponível
+                    </p>
+                  </div>
+                )}
               </div>
 
               <div className='grid grid-cols-1 md:grid-cols-3 gap-3 w-full'>
