5e978f158b0bee4d6ee2a5c99eadeddca4c72a0b
Julia Fazenda Ruiz <juliafazendaruiz@gmail.com>
2025-06-14 15:34:26 -0300

    prod

--- services/api.js
+++ services/api.js
@@ -1,39 +1,39 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL;
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL
 
 export const api = axios.create({
   baseURL: baseURL,
-});
+})
 
 if (typeof window !== 'undefined') {
   api.interceptors.request.use(
     config => {
-      const token = sessionStorage.getItem('token');
+      const token = sessionStorage.getItem('token')
 
       if (
         !config.url.includes('/login') &&
         !config.url.includes('/create-user') &&
         token
       ) {
-        config.headers.Authorization = `Bearer ${token}`;
+        config.headers.Authorization = `Bearer ${token}`
       }
 
-      return config;
+      return config
     },
     error => {
-      return Promise.reject(error);
+      return Promise.reject(error)
     }
-  );
+  )
 
   api.interceptors.response.use(
     response => response,
     error => {
       if (error.response && error.response.status === 401) {
         // Redireciona para o login em caso de erro 401
-        window.location.href = '/login';
+        window.location.href = '/login'
       }
-      return Promise.reject(error);
+      return Promise.reject(error)
     }
-  );
+  )
 }
